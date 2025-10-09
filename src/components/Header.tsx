5a7740f03bfa0e2a22a3aa0c4d6374077a2609db
Ashutosh Biswas <ashutoshbw314@gmail.com>
2025-10-10 01:21:15 +0600

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -37,7 +37,7 @@ export function Header({ color }: { color: string }) {
             href="/"
             onClick={() => setIsOpen(false)}
           >
-            Uthpala Biswas
+            Uthpala Biswas {pathname}
           </Link>
           <div
             className={cn(
